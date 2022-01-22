d435a116313c86483f0dbe52bed397438742dd8b
andz <hi@an-dz.me>
2022-01-22 21:54:02 +0000

    try and fix eslint and tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,4 @@
-const tailwindcss = require('tailwindcss');
-module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
-};
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+export const plugins = [tailwindcss("./tailwind.config.js"), autoprefixer];
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import Theme from './controls/theme';
+import React from "react";
+import Theme from "./controls/theme";
 
 class App extends React.Component {
+  get day(): number {
+    const dayOne = new Date("2021/06/20");
+    const diff = new Date().getTime() - dayOne.getTime();
+    return Math.ceil(diff / (1000 * 3600 * 24));
+  }
 
   render(): React.ReactNode {
     return (
-      <div className='text-center dark:bg-slate-800'>
-        <Theme />
-          <div className='bg-red-800 text-slate-900 dark:text-white'>
-            Cheatle
+      <div className="bg-gray-200 dark:bg-gray-900">
+        <div className="container mx-auto text-center">
+          <Theme />
+          <div className="bg-purple-700 text-slate-900 dark:text-white">
+            Cheatle day {this.day}
           </div>
+        </div>
       </div>
     );
   }
