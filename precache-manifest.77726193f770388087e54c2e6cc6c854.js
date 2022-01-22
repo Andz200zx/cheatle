e8da71b31028754be82bc0eb411a6925fba70007
andz <hi@an-dz.me>
2022-01-22 15:59:46 +0000

    Updates

---
+++ precache-manifest.77726193f770388087e54c2e6cc6c854.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd5ab24967286fbab54c2165f611420",
+    "url": "/cheatle/index.html"
+  },
+  {
+    "revision": "081d5f7c27a44ff5033f",
+    "url": "/cheatle/static/css/main.aa08f369.chunk.css"
+  },
+  {
+    "revision": "42d8e8859104679ce821",
+    "url": "/cheatle/static/js/2.bc0e5e72.chunk.js"
+  },
+  {
+    "revision": "081d5f7c27a44ff5033f",
+    "url": "/cheatle/static/js/main.8e287c06.chunk.js"
+  },
+  {
+    "revision": "d8b2794248f97d0bb38a",
+    "url": "/cheatle/static/js/runtime~main.be8d7063.js"
+  }
+]);
